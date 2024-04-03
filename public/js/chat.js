e0024cac73cbeb9ff70e232f88db299c9bba54c9
Raphael Ferras <raphaelferras@Raphaels-MacBook-Pro.local>
2024-04-03 09:41:28 -0400

    Mustache templates

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,53 @@
 const socket = io()
 
-const chatForm = document.querySelector('#messageForm')
-const sendLocationButton = document.querySelector('#sendLocation')
+const $messageForm = document.querySelector('#messageForm')
+const $messageFormInput = messageForm.querySelector('input')
+const $messageFormButton = messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#sendLocation')
+const $messagesContainer = document.querySelector('#messages')
+
+// Templates
+const $messageTemplate = document.querySelector('#message-template').innerHTML
+const $locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(`message: ${message}`)
+    const html = Mustache.render($messageTemplate, {
+        message
+    })
+    $messagesContainer.insertAdjacentHTML('beforeend', html)
 })
 
-chatForm.addEventListener('submit', (e) => {
+socket.on('locationMessage', (link) => {
+    console.log(`locationMessage: ${link}`)
+    const html = Mustache.render($locationMessageTemplate, {
+        link
+    })
+    $messagesContainer.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const message = e.target.elements.messageInput.value
-    e.target.elements.messageInput.value = ''
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = $messageFormInput.value
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+
         if (error) {
             return console.log('error')
         }
+
         console.log('the message was received')
     })
 })
 
-sendLocationButton.addEventListener('click', (e) => {
+$sendLocationButton.addEventListener('click', (e) => {
     e.preventDefault()
+    $sendLocationButton.setAttribute('disabled', 'disabled')
 
     if (!navigator.geolocation) {
         return alert('geolocation is not supportted by your browser')
@@ -31,6 +58,7 @@ sendLocationButton.addEventListener('click', (e) => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocationButton.removeAttribute('disabled')
             console.log('location was shared correctly')
         })
     })
